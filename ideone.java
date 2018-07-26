e496a0e2cd562cd2f3ac604e4a0f92ce7cd27753
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-07-26 18:59:42 +0530

    ideone.java

---
+++ ideone.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+        int num1=8;
+        int num2=-5;
+        int number;
+        number=num1+num2;
+        if(number > 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+    }
+}
+
+	
